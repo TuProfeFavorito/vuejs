844cb9d1d3e7f72da031a7bec5d1860bfff3f940
301-MODULO <rdelarosa@uch.edu.pe>
2024-05-29 19:40:07 -0500

    first comit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Esta es mi primera vez en Vue',
+        see: true,
+        pollerias: [
+            { nombre: 'Pardos' },
+            { nombre: 'Norkys' },
+            { nombre: 'Kykos' },
+            { nombre: 'La Leña' }
+        ]
+    }
+})
